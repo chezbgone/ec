1394d553b173c8f2c6f05dab8cb3f9dc8deace21
Jason Chen <chezbgone@gmail.com>
2020-06-11 03:47:10 -0500

    Combine subtasks of list-harder so that each task is actually one task

--- dreamcoder/domains/list/main.py
+++ dreamcoder/domains/list/main.py
@@ -276,7 +276,7 @@ def main(args):
         "Lucas-depth1": lambda: retrieveJSONTasks("data/list_tasks2.json")[:105],
         "Lucas-depth2": lambda: retrieveJSONTasks("data/list_tasks2.json")[:4928],
         "Lucas-depth3": lambda: retrieveJSONTasks("data/list_tasks2.json"),
-        "custom-harder": lambda: retrieveJSONTasks("data/custom_harder_tasks.json"),
+        "custom-harder": lambda: retrieveJSONTasks("data/custom_harder_tasks_joined.json"),
     }[dataset]()
 
     maxTasks = args.pop("maxTasks")
